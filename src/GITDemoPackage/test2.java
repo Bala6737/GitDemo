611128a8d5239512e0849fad9d703d4fabc2a60d
Mokshi <Mokshi@LAPTOP-QVMBRHR1>
2022-05-19 11:38:35 +0530

    Commiting test2 to Branch1

---
+++ src/GITDemoPackage/test2.java
@@ -0,0 +1,20 @@
+package GITDemoPackage;
+
+public class test2 {
+
+	public static void main(String[] args) {
+	
+		
+		
+		String s = "hungry";
+		
+		if(s.equals("hungry")) {
+			System.out.println("eat something");
+		}
+		else {
+			System.out.println("Do work");
+		}
+
+	}
+
+}
